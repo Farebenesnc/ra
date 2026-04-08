64a9234213091f91ffd313005a7f03b5bbf37f42
Farebenesnc <farebenesnc@gmail.com>
2026-04-08 11:05:16 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+// Service Worker che si auto-disinstalla per forzare aggiornamento
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(names.map(function(name) {
+        return caches.delete(name);
+      }));
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
+self.addEventListener('fetch', function(e) {
+  // Nessuna cache - sempre rete
+  e.respondWith(fetch(e.request));
+});
